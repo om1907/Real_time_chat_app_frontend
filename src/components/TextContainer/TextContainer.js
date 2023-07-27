498963128ebd479bb0287845d7f1c44626d78155
om1907 <omkarkumar1111@gmail.com>
2023-07-27 08:50:48 +0530

    initial

---
+++ src/components/TextContainer/TextContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">    
+   
+  </div>
+);
+
+export default TextContainer;
